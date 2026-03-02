84d1e26bdbdb5086e0dfbb45eab4247bc9fcee14
Ulisses da Silva Calado <ulisses.scalado@gmail.com>
2026-03-02 16:09:17 -0300

    descricao caged

--- ecodatagruapi/rotas/caged.py
+++ ecodatagruapi/rotas/caged.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Query
+from fastapi import APIRouter, Request, Query, HTTPException
 from app.schemas.caged_schema import CagedSchema
 from app.dao.caged_dao import CagedDAO
 from typing import List
@@ -9,12 +9,20 @@ router = APIRouter(prefix="/v1/caged", tags=["Caged"])
 @router.get("/caged_setor", response_model=List[CagedSchema],
             summary="Consulta dados referentes ao município.",
             description="""
-            Para realizar a consulta, escolhar um setor entre: industria, comercio, servico, agricultura.
+            Para realizar a consulta, escolha um setor entre: industria, comercio, servico, agricultura.
             """)
 @limiter.limit("5/minute")
 def get_caged_setor(request: Request, setor: str):
-    return CagedDAO.get_por_setor(setor)
-
+    if setor.lower() == 'industria':
+        return CagedDAO.get_por_setor(setor)
+    if setor.lower() == "comercio":
+        return CagedDAO.get_por_setor(setor)
+    if setor.lower() == "servico":
+        return CagedDAO.get_por_setor(setor)
+    elif setor.lower() == "agricultura":
+        return CagedDAO.get_por_setor(setor)
+    return HTTPException(status_code=400, detail="Setor não encontrado. escolha um setor entre: industria, comercio, "
+                                                 "servico, agricultura.")
 
 @router.get("/caged_ano", response_model=List[CagedSchema],
             summary="Consulta dados referentes ao município.",
